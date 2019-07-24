fd096f7f6914d60514888778cd20d79de3d6e071
proforg <proforg@maloletka.ru>
2019-07-24 11:23:18 +0300

    enable use of account_id on initialization

--- lib/huobi_client.rb
+++ lib/huobi_client.rb
@@ -3,8 +3,8 @@ require "huobi_client/version"
 
 module HuobiClient
   class << self
-    def new(access, secret)
-      HuobiClient::Client.new(access, secret)
+    def new(access, secret, account_id=nil)
+      HuobiClient::Client.new(access, secret, account_id)
     end
   end
 end
