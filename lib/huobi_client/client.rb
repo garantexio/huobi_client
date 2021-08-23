9900b89cfd203e0907f3048317ff742347c2d6d1
proforg <proforg@maloletka.ru>
2021-08-24 00:56:02 +0300

    make currency optional to deposit-withdraw query

--- lib/huobi_client/client.rb
+++ lib/huobi_client/client.rb
@@ -159,7 +159,7 @@ module HuobiClient
       post '/v1/dw/withdraw-virtual/{withdraw-id}/cancel'
     end
 
-    def withdraw_query(currency:, type:, from: nil, size: nil, direct: nil) # 查询虚拟币充提记录
+    def withdraw_query(currency: nil, type:, from: nil, size: nil, direct: 'next') # 查询虚拟币充提记录
       # type	true	string	'deposit' or 'withdraw'
       get '/v1/query/deposit-withdraw', fun_params(__method__, binding)
     end
